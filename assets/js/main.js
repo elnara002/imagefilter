9b92ce5f1e07063d2c9c85152d2e941133b50efd
elnara002 <6fdtxsg@code.edu.az>
2023-05-12 23:56:06 +0400

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,18 @@
-const container = document.querySelector(".container");
-const buttons = document.querySelectorAll("button");
-const images = document.querySelectorAll("img");
-
-function flowerSelect() {
-  const flowertype = this.getAttribute("flowertype");
-  for (let j = 0; j < images.length; j++) {
-    if (images[j].getAttribute("flowertype") === flowertype || flowertype === "All") {
-      images[j].style.display = "block";
-    } else {
-      images[j].style.display = "none";
-    } 
+  function flowerSelect(btn) {
+    const selectedFlowerType = btn.textContent;
+    const flowerImages = document.querySelectorAll('img[flowertype]');
+    for (let i = 0; i < flowerImages.length; i++) {
+      const flowerType = flowerImages[i].getAttribute('flowertype');
+      if (selectedFlowerType === 'All' || selectedFlowerType === flowerType) {
+        flowerImages[i].style.display = 'block';
+      } else {
+        flowerImages[i].style.display = 'none';
+      }
+    }
   }
-}
-
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", flowerSelect);
-}
-  // Not:Işləmir amma yenə də atdım)
-
-
+  
+  
+  
 
 
 
